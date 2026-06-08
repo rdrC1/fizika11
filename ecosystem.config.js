deeb070dccdf460b3acffa5f2fe6db5695822539
rdrC1 <farkas.zsombor.live@gmail.com>
2026-06-08 17:55:36 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,12 +6,12 @@ module.exports = {
       watch: false,
       env: {
         PM2_SERVE_PATH: ".",
-        PM2_SERVE_PORT: 8080,
+        PM2_SERVE_PORT: 34927,
         PM2_SERVE_SPA: "true",
         PM2_SERVE_HOMEPAGE: "./index.html"
       },
       env_production: {
-        PM2_SERVE_PORT: 8080
+        PM2_SERVE_PORT: 34927
       }
     }
   ]
